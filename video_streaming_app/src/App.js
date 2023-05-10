0fb5381a8b173d1e828db61c91574e348900e5b6
Rohanpatil22 <rohanpatil1797@gmail.com>
2023-05-10 22:06:18 +0530

    Basic react app structure

---
+++ video_streaming_app/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+ function App() {
+
+//  async function get_info()
+//   {
+//     const getdata=await fetch("https://raw.githubusercontent.com/bikashthapa01/myvideos-android-app/master/data.json")
+//     .then((res)=>{
+//       console.log(res);
+//     })
+//   }
+
+  return (
+    <>
+    {/* <div onClick={get_info}>click</div> */}
+      </>
+  );
+}
+
+export default App;
