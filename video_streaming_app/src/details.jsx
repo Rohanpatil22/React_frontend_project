be28e4216bfcf4ede620ba2750c01b86813952c0
Rohanpatil22 <rohanpatil1797@gmail.com>
2023-07-27 23:21:10 +0530

    embed youtube video added

--- video_streaming_app/src/details.jsx
+++ video_streaming_app/src/details.jsx
@@ -8,21 +8,35 @@ function MovieDetails(props){
     const[flag,setflag]=useState(true);
     const[deatilsData,setDetailsData]=useState('');
     const [movieIdSel,setMovieIdsel]=useState(props.selMovieId);
+    const[videoPath,setvideopath]=useState('');
     let base_img_url="https://www.themoviedb.org/t/p/w66_and_h66_face";
+    // let iframe_src=`https://www.themoviedb.org/video/play?key=${videoPath}`;
+    let iframe_src=`https://www.youtube.com/embed/${videoPath}?autoplay=1&mute=1`;
     console.log(`https://api.themoviedb.org/3/movie/${props.selMovieId}/credits?language=en-US&api_key=c72ff33d0282fe5abd382b77ceaff037`);
 
-    function getCastData()
+    async function getCastData()
     {
-        const castData= axios.get(`https://api.themoviedb.org/3/movie/${props.selMovieId}/credits?language=en-US&api_key=c72ff33d0282fe5abd382b77ceaff037`)
-        .then((res)=>{
-            let data=res.data;
-            console.log(data);
-            setDetailsData(data);
-        })  
-        .catch((err)=>{
-           
-        })
+        const castData=await axios.get(`https://api.themoviedb.org/3/movie/${props.selMovieId}/credits?language=en-US&api_key=c72ff33d0282fe5abd382b77ceaff037`)
+
+        console.log(castData.data);
+        setDetailsData(castData.data);
+
+        const video_key=await axios.get(`https://api.themoviedb.org/3/movie/${props.selMovieId}/videos`, {headers:{
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzJmZjMzZDAyODJmZTVhYmQzODJiNzdjZWFmZjAzNyIsInN1YiI6IjY0NWU3ZGFiOTU5MGUzMDEwMTZjMzQwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.09TEi_EkHdrvkhMMXDeRH65wct6uMBmP8MMaw-QVJWI'
+          }})
+        
+         
+          video_key.data.results.map((item)=>{
+            if(item.type==="Trailer")
+            {
+                setvideopath(item.key);
+            }
+          })
+          
     }
+
+   
    
    useEffect(()=>{
     getCastData();
@@ -32,11 +46,12 @@ function MovieDetails(props){
     {
         return(
             <>
-            <div className="text-right text-white"><button className="mr-10" onClick={()=>{if(flag){setflag(false)}else{setflag(true)}}}>{!flag &&<FontAwesomeIcon style={{fontSize:"30px"}} icon={faGrip} />}{flag &&<FontAwesomeIcon style={{fontSize:"30px"}} icon={faList} /> }</button></div>
+            <div className="m-20 " ><iframe height={600} width="100%" className=" m-auto top-0 left" src={iframe_src} allowFullScreen /></div>
+            <div className="text-right text-white"><button className="mr-32" onClick={()=>{if(flag){setflag(false)}else{setflag(true)}}}>{!flag &&<FontAwesomeIcon style={{fontSize:"30px"}} icon={faGrip} />}{flag &&<FontAwesomeIcon style={{fontSize:"30px"}} icon={faList} /> }</button></div>
             <div   className={flag? "grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-10 w-11/12 m-auto bg-zinc-800 p-10":"m-auto gap-10 bg-zinc-800 p-10"}>
             { flag &&
                 deatilsData.cast.map((item)=>(
-                  (item.profile_path!==null)&& <div key={item.name}>
+                  (item.profile_path!==null)&& <div key={item.id}>
                         <div>{item.title}</div>
                         <div className="flex justify-center"  ><img className="rounded-full text-center" alt={item.name} style={{width:"90px",textAlign:"center"}} src={base_img_url+item.profile_path}></img></div>  
                         <div className="text-gray-50 text-center text-lg" >{item.name}</div>
@@ -50,7 +65,7 @@ function MovieDetails(props){
             }
              { !flag && 
                 deatilsData.cast.map((item)=>(
-                  (item.profile_path!==null)&& <div key={item.name}>
+                  (item.profile_path!==null)&& <div key={item.id}>
                         <div className="flex justify-start gap-4 md:gap-20 items-center  w-11/10 sm:w-3/5 lg:w-1/2 m-auto md:p-6 p-2">
                         <div>{item.title}</div>
                         <div className="text-left"><img className="rounded-full " alt={item.name} style={{width:"90px"}} src={base_img_url+item.profile_path}></img></div>  
